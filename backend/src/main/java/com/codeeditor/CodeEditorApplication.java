4372a275e3114f35e10e007de4b53575bae9553b
Kratika <84068268+kratikachaudhary298@users.noreply.github.com>
2026-05-07 23:17:45 +0530

    Add Spring Boot main application class

---
+++ backend/src/main/java/com/codeeditor/CodeEditorApplication.java
@@ -0,0 +1,12 @@
+package com.codeeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeEditorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeEditorApplication.class, args);
+    }
+}
